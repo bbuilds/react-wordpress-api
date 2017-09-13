c4b8ce159d7f1f595425cd279d6dc8ae1b34de87
Branden Builds <hello@brandenbuilds.com>
2017-09-13 13:50:49 -0400

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,61 @@
+const baseUrl = 'http://wordpresslinkhere.com/wp-json/wp/v2/';
+const authUrl = 'http://wordpresslinkhere.com/wp-json/jwt-auth/v1'
+export default class Api {
+
+    posts(options = {}) {
+        let url = `${baseUrl}posts`;
+        
+        if(options.id !== undefined ) {
+	        url += `/${options.id}`;
+        }
+        
+        url += '?_embed';
+        
+        if(options.category !== undefined) {
+	         url += `&categories=${options.category}`;
+        }
+        
+        return fetch(url).then(res => res.json());
+    }
+    
+    categories() {
+	    let url = `${baseUrl}categories`;
+        
+        return fetch(url).then(res => res.json());
+	    
+    }    
+    
+     authenticate(username, password) {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        return fetch(`${authUrl}/token`, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({username, password})
+        }).then(res => {
+            if (!res.ok) {
+                throw Error(res.statusText);
+            }
+
+            return res.json();
+        });
+    }
+    
+    
+   deletePost(token, id) {
+        const headers = new Headers();
+        headers.append('Authorization', `Bearer ${token}`);
+
+        return fetch(`${baseUrl}posts/${id}`, {
+            method: 'DELETE',
+            headers
+        }).then(res => {
+            if (!res.ok) {
+                throw Error(res.statusText);
+            }
+
+            return res.json();
+        });
+    }
+}
